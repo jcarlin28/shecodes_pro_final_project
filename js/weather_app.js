581c1bc4e5fca4defc7517b2654a46775f0485bc
jcarlin28 <117719590+jcarlin28@users.noreply.github.com>
2022-12-01 18:32:58 -0800

    Removed hourly forecast and map image

--- js/weather_app.js
+++ js/weather_app.js
@@ -28,32 +28,47 @@ let newMinutes = String(now.getMinutes()).padStart(2, "0");
 
 timeDate.innerHTML = `${newDay} / ${newHours}:${newMinutes}`;
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
   let forecastHTML = `<div class="row">`;
-  forecastHTML =
-    forecastHTML +
-    `<div class="col-2">
-            <div class="dailyForecast" id="forecast">Sunday
-              <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
-                  class="dailyIcon"
-                  alt=""
-                  width="42"
-                /> 
-                <div class="daily-forecast-temp">
-          <span class="forecast-temp-max">18° | </span>
-          <span class="forecast-temp-min">10°</span>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° | </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
-      </div> 
-          </div>
-        </div>`;
+      </div>
+  `;
+    }
+  });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-displayForecast();
 
 function search(event) {
   event.preventDefault();
@@ -94,6 +109,13 @@ function showTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
   );
+  console.log(response.data);
+  getForecast(response.data.coord);
+}
+function getForecast(coordinates) {
+  let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let realTemperature = null;
